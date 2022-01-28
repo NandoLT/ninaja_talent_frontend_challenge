129792744f8957f0defeb024884070964b825001
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-01-28 23:11:37 +0100

    Some adjustmen in Header component and css

--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -6,16 +6,9 @@ import '../../assets/css/header.css';
 const Header = () => {
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="/">
-                    <img src={ ninjatalent_logo } alt="ninjatalent-logo-img" width="112" height="28" />
-                </a>
-                <a href="/" role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
+            <a className="navbar-item" href="/">
+                <img src={ ninjatalent_logo } alt="ninjatalent-logo-img" width="112" height="28" />
+            </a>
         </nav>
     )
 }
